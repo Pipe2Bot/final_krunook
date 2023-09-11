cc016a56414ec5f8bfb3c0ed6c000fa182b1b2e7
Thatchaphum Jaidee <134765926+Pipe2Bot@users.noreply.github.com>
2023-09-11 21:35:24 +0700

    Add files via upload

---
+++ FinalProject_1-3/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FinalProject_1_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbsub1 = new Label();
+            ddlmain = new ComboBox();
+            lbsub2 = new Label();
+            label1 = new Label();
+            bttgo = new Button();
+            lbsub3 = new Label();
+            tbsub1 = new TextBox();
+            tbsub3 = new TextBox();
+            tbsub2 = new TextBox();
+            SuspendLayout();
+            // 
+            // lbsub1
+            // 
+            lbsub1.AutoSize = true;
+            lbsub1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            lbsub1.Location = new Point(57, 177);
+            lbsub1.Name = "lbsub1";
+            lbsub1.Size = new Size(107, 41);
+            lbsub1.TabIndex = 0;
+            lbsub1.Text = "lbsub1";
+            // 
+            // ddlmain
+            // 
+            ddlmain.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            ddlmain.FormattingEnabled = true;
+            ddlmain.Items.AddRange(new object[] { "1 สี่เหลี่ยมผืนผ้า", "2 วงกลม", "3 สามเหลี่ยม", "4 สี่เหลี่ยมรูปว่าว", "5 สี่เหลี่ยมด้านขนาน" });
+            ddlmain.Location = new Point(389, 31);
+            ddlmain.Name = "ddlmain";
+            ddlmain.Size = new Size(222, 49);
+            ddlmain.TabIndex = 1;
+            ddlmain.SelectedIndexChanged += ddlmain_SelectedIndexChanged;
+            // 
+            // lbsub2
+            // 
+            lbsub2.AutoSize = true;
+            lbsub2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            lbsub2.Location = new Point(57, 267);
+            lbsub2.Name = "lbsub2";
+            lbsub2.Size = new Size(107, 41);
+            lbsub2.TabIndex = 2;
+            lbsub2.Text = "lbsub2";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(57, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(326, 41);
+            label1.TabIndex = 3;
+            label1.Text = "เลือกรูปเรขาคณิตที่ต้องการ";
+            // 
+            // bttgo
+            // 
+            bttgo.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            bttgo.Location = new Point(742, 190);
+            bttgo.Name = "bttgo";
+            bttgo.Size = new Size(294, 94);
+            bttgo.TabIndex = 4;
+            bttgo.Text = "คำนวณ";
+            bttgo.UseVisualStyleBackColor = true;
+            bttgo.Click += bttgo_Click;
+            // 
+            // lbsub3
+            // 
+            lbsub3.AutoSize = true;
+            lbsub3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            lbsub3.Location = new Point(57, 359);
+            lbsub3.Name = "lbsub3";
+            lbsub3.Size = new Size(107, 41);
+            lbsub3.TabIndex = 5;
+            lbsub3.Text = "lbsub3";
+            // 
+            // tbsub1
+            // 
+            tbsub1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            tbsub1.Location = new Point(311, 177);
+            tbsub1.Name = "tbsub1";
+            tbsub1.Size = new Size(177, 47);
+            tbsub1.TabIndex = 6;
+            // 
+            // tbsub3
+            // 
+            tbsub3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            tbsub3.Location = new Point(311, 353);
+            tbsub3.Name = "tbsub3";
+            tbsub3.Size = new Size(177, 47);
+            tbsub3.TabIndex = 8;
+            // 
+            // tbsub2
+            // 
+            tbsub2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            tbsub2.Location = new Point(311, 267);
+            tbsub2.Name = "tbsub2";
+            tbsub2.Size = new Size(177, 47);
+            tbsub2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1093, 521);
+            Controls.Add(tbsub2);
+            Controls.Add(tbsub3);
+            Controls.Add(tbsub1);
+            Controls.Add(lbsub3);
+            Controls.Add(bttgo);
+            Controls.Add(label1);
+            Controls.Add(lbsub2);
+            Controls.Add(ddlmain);
+            Controls.Add(lbsub1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbsub1;
+        private ComboBox ddlmain;
+        private Label lbsub2;
+        private Label label1;
+        private Button bttgo;
+        private Label lbsub3;
+        private TextBox tbsub1;
+        private TextBox tbsub3;
+        private TextBox tbsub2;
+    }
+}
---
+++ FinalProject_1-3/Form1.cs
@@ -0,0 +1,248 @@
+﻿namespace FinalProject_1_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbsub1.Visible = false;
+            lbsub2.Visible = false;
+            lbsub3.Visible = false;
+        }
+
+        private void bttgo_Click(object sender, EventArgs e)
+        {
+            if (ddlmain.Text != "")
+            {
+                
+                if (ddlmain.SelectedIndex == 0) // 4 ผืนผ้า
+                {
+                    double leng, wide, area;
+                    /*
+                    wide = Convert.ToDouble(lbsub1.Text);
+                    leng = Convert.ToDouble(lbsub2.Text);
+                    area = Convert.ToDouble(lbsub3.Text);
+                    area = wide * leng;
+                    wide = area / leng;
+                    leng = area / wide;
+                    */
+                    if (tbsub1.Text == "")
+                    {
+                        leng = Convert.ToDouble(tbsub2.Text);
+                        area = Convert.ToDouble(tbsub3.Text);
+                        wide = area / leng;
+                        tbsub1.Text = wide.ToString();
+                    }
+                    else if (tbsub2.Text == "")
+                    {
+                        wide = Convert.ToDouble(tbsub1.Text);
+                        area = Convert.ToDouble(tbsub3.Text);
+                        leng = area / wide;
+                        tbsub2.Text = leng.ToString();
+                    }
+                    else
+                    {
+                        wide = Convert.ToDouble(tbsub1.Text);
+                        leng = Convert.ToDouble(tbsub2.Text);
+                        area = wide * leng;
+                        tbsub3.Text = area.ToString();
+                    }
+
+
+
+
+                }
+                else if (ddlmain.SelectedIndex == 1) //circle
+                {
+                    double rad, area;
+
+                    lbsub1.Visible = true;
+                    lbsub2.Visible = true;
+                    lbsub3.Visible = false;
+
+                    if (tbsub1.Text == "")
+                    {
+                        area = Convert.ToDouble(tbsub2.Text);
+                        rad = (area / 22 * 7);
+                        rad = Math.Sqrt(rad);
+                        tbsub1.Text = rad.ToString();
+
+                    }
+                    else if (tbsub2.Text == "")
+                    {
+                        rad = Convert.ToDouble(tbsub1.Text);
+                        area = rad * rad * 22 / 7;
+                        tbsub2.Text = area.ToString();
+                    }
+                }
+                else if (ddlmain.SelectedIndex == 2)
+                {
+                    double leng, wide, area;
+                    /*
+                    wide = Convert.ToDouble(lbsub1.Text);
+                    leng = Convert.ToDouble(lbsub2.Text);
+                    area = Convert.ToDouble(lbsub3.Text);
+                    area = wide * leng;
+                    wide = area / leng;
+                    leng = area / wide;
+                    */
+                    if (tbsub1.Text == "")
+                    {
+                        leng = Convert.ToDouble(tbsub2.Text);
+                        area = Convert.ToDouble(tbsub3.Text);
+                        wide = area * 2 / leng;
+                        tbsub1.Text = wide.ToString();
+                    }
+                    else if (tbsub2.Text == "")
+                    {
+                        wide = Convert.ToDouble(tbsub1.Text);
+                        area = Convert.ToDouble(tbsub3.Text);
+                        leng = area * 2 / wide;
+                        tbsub2.Text = leng.ToString();
+                    }
+                    else
+                    {
+                        wide = Convert.ToDouble(tbsub1.Text);
+                        leng = Convert.ToDouble(tbsub2.Text);
+                        area = wide * leng / 2;
+                        tbsub3.Text = area.ToString();
+                    }
+                }
+                else if (ddlmain.SelectedIndex == 3)
+                {
+                    double leng, wide, area;
+                    /*
+                    wide = Convert.ToDouble(lbsub1.Text);
+                    leng = Convert.ToDouble(lbsub2.Text);
+                    area = Convert.ToDouble(lbsub3.Text);
+                    area = wide * leng;
+                    wide = area / leng;
+                    leng = area / wide;
+                    */
+                    if (tbsub1.Text == "")
+                    {
+                        leng = Convert.ToDouble(tbsub2.Text);
+                        area = Convert.ToDouble(tbsub3.Text);
+                        wide = area * 2 / leng;
+                        tbsub1.Text = wide.ToString();
+                    }
+                    else if (tbsub2.Text == "")
+                    {
+                        wide = Convert.ToDouble(tbsub1.Text);
+                        area = Convert.ToDouble(tbsub3.Text);
+                        leng = area * 2 / wide;
+                        tbsub2.Text = leng.ToString();
+                    }
+                    else
+                    {
+                        wide = Convert.ToDouble(tbsub1.Text);
+                        leng = Convert.ToDouble(tbsub2.Text);
+                        area = wide * leng / 2;
+                        tbsub3.Text = area.ToString();
+                    }
+                }
+                else if (ddlmain.SelectedIndex == 4) // 4 ผืนผ้า
+                {
+                    double leng, wide, area;
+                    /*
+                    wide = Convert.ToDouble(lbsub1.Text);
+                    leng = Convert.ToDouble(lbsub2.Text);
+                    area = Convert.ToDouble(lbsub3.Text);
+                    area = wide * leng;
+                    wide = area / leng;
+                    leng = area / wide;
+                    */
+                    if (tbsub1.Text == "")
+                    {
+                        leng = Convert.ToDouble(tbsub2.Text);
+                        area = Convert.ToDouble(tbsub3.Text);
+                        wide = area / leng;
+                        tbsub1.Text = wide.ToString();
+                    }
+                    else if (tbsub2.Text == "")
+                    {
+                        wide = Convert.ToDouble(tbsub1.Text);
+                        area = Convert.ToDouble(tbsub3.Text);
+                        leng = area / wide;
+                        tbsub2.Text = leng.ToString();
+                    }
+                    else
+                    {
+                        wide = Convert.ToDouble(tbsub1.Text);
+                        leng = Convert.ToDouble(tbsub2.Text);
+                        area = wide * leng;
+                        tbsub3.Text = area.ToString();
+                    }
+
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ddlmain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlmain.SelectedIndex == 0) // 4 ผืนผ้า
+            {
+                lbsub1.Visible = true;
+                lbsub2.Visible = true;
+                lbsub3.Visible = true;
+                tbsub3.Visible = true;
+                lbsub1.Text = "กว้าง";
+                lbsub2.Text = "ยาว";
+                lbsub3.Text = "พื้นที่";
+
+            }
+            else if (ddlmain.SelectedIndex == 1) //circle
+            {
+                lbsub1.Visible = true;
+                lbsub2.Visible = true;
+                lbsub3.Visible = false;
+                tbsub3.Visible = false;
+                lbsub1.Text = "รัศมี";
+                lbsub2.Text = "พื้นที่";
+            }
+            else if (ddlmain.SelectedIndex == 2) // 4 ผืนผ้า
+            {
+                lbsub1.Visible = true;
+                lbsub2.Visible = true;
+                lbsub3.Visible = true;
+                tbsub3.Visible = true;
+                lbsub1.Text = "ยาว";
+                lbsub2.Text = "สูง";
+                lbsub3.Text = "พื้นที่";
+
+            }
+            else if (ddlmain.SelectedIndex == 3) // 4 ผืนผ้า
+            {
+                lbsub1.Visible = true;
+                lbsub2.Visible = true;
+                lbsub3.Visible = true;
+                tbsub3.Visible = true;
+                lbsub1.Text = "เส้นทะเเยงมุม 1";
+                lbsub2.Text = "เส้นทะเเยงมุม 2";
+                lbsub3.Text = "พื้นที่";
+
+            }
+            else if (ddlmain.SelectedIndex == 4) // 4 ผืนผ้า
+            {
+                lbsub1.Visible = true;
+                lbsub2.Visible = true;
+                lbsub3.Visible = true;
+                tbsub3.Visible = true;
+                lbsub1.Text = "ฐาน";
+                lbsub2.Text = "สูง";
+                lbsub3.Text = "พื้นที่";
+
+
+            }
+        }
+
+      
+
+    }
+}
